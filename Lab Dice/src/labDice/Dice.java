e207eed97eee9f3ad80a67fc8bff9e5c027eb547
Pauline Makoma <uwasepaldy@gmail.com>
2019-10-07 22:13:54 -0600

    Lab Dice finished

--- Lab Dice/src/labDice/Dice.java
+++ Lab Dice/src/labDice/Dice.java
@@ -2,15 +2,20 @@ package labDice;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Dice extends JFrame {
 
@@ -42,24 +47,47 @@ public class Dice extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+
 		
-//		Icon [] icons = { "file:///Users/paulinemakoma/Downloads/dice/die-1.png", 
-//					"file:///Users/paulinemakoma/Downloads/dice/die-2.png", 
-//					"file:///Users/paulinemakoma/Downloads/dice/die-3.png", 
-//					"file:///Users/paulinemakoma/Downloads/dice/die-4.png", 
-//					"file:///Users/paulinemakoma/Downloads/dice/die-5.png", 
-//					"file:///Users/paulinemakoma/Downloads/dice/die-6.png" };
+		
+		JLabel lblNewLabel = new JLabel("");
+		Image img1 = new ImageIcon(this.getClass().getResource("/die-1.png")).getImage();
+		Image img2 = new ImageIcon(this.getClass().getResource("/die-2.png")).getImage();
+		Image img3 = new ImageIcon(this.getClass().getResource("/die-3.png")).getImage();
+		Image img4 = new ImageIcon(this.getClass().getResource("/die-4.png")).getImage();
+		Image img5 = new ImageIcon(this.getClass().getResource("/die-5.png")).getImage();
+		Image img6 = new ImageIcon(this.getClass().getResource("/die-6.png")).getImage();
+		
+		Random rand = new Random();
+		
 		
 		JButton btnRoll = new JButton("ROLL");
 		btnRoll.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				///rolling the dice event will occur here.
-				
+				int n = rand.nextInt(6)+1;
+				if (n == 1) {
+					lblNewLabel.setIcon(new ImageIcon(img1));
+				} else if (n == 2) {
+					lblNewLabel.setIcon(new ImageIcon(img2));
+				} else if ( n ==3 ) {
+					lblNewLabel.setIcon(new ImageIcon(img3));
+				} else if( n == 4) {
+					lblNewLabel.setIcon(new ImageIcon(img4));
+				}else if( n ==5 ) {
+					lblNewLabel.setIcon(new ImageIcon(img5));
+				} else if (n ==6){
+					lblNewLabel.setIcon(new ImageIcon(img6));
+				} else 
+					System.out.println(" nothing");
 			}
 		});
 		btnRoll.setForeground(Color.RED);
 		contentPane.add(btnRoll, BorderLayout.SOUTH);
+		
+		
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel, BorderLayout.CENTER);
 		
 		
 	}
