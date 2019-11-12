ef0f54bd0be13009b4aca2812f1aa62a90979c59
Pauline Makoma <uwasepaldy@gmail.com>
2019-11-11 17:19:16 -0700

    lab exception changed

--- Lab Exception/src/exception/LabException.java
+++ Lab Exception/src/exception/LabException.java
@@ -3,6 +3,7 @@
  */
 package exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,15 +16,36 @@ public class LabException {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int digit = numberFromUser();
-		int result = sevenModulusN(digit);
-		System.out.printf("7 %% %.1f = %.1f", digit, result);
+		  try {
+	            int digit = numberFromUser();
+
+	            int result = sevenModulusN(digit);
+	            System.out.printf("7 %% %d = %d", digit, result);
+	        } catch (IllegalArgumentException exception) {
+
+	            System.out.println(exception.getMessage());
+	        }
 	}
 	
 	private static int numberFromUser(){
 		Scanner input = new Scanner(System.in);
-		System.out.print("number: ");
-		return input.nextInt();
+        while (true) {
+            try {
+
+                System.out.print("number: ");
+                int number = input.nextInt();
+                input.nextLine();
+            
+                if (number == 0) 
+                	throw new IllegalArgumentException("Can’t calculate 7 % 0 ");
+                return number;
+                
+            } catch (InputMismatchException e) {
+                System.out.println("The number entered needs to be a whole number");
+                input.nextLine();
+            }
+
+        }
 	}
 	
 	private static int sevenModulusN(int number){
