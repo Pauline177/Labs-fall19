c9983a6e526cb2cd5d114cd5ac1f120b25985311
Pauline Makoma <uwasepaldy@gmail.com>
2019-11-17 00:00:05 -0700

    A04 inheritance completed

---
+++ A04 Inheritance/src/inheritance/IsoscelesRightTriangle.java
@@ -0,0 +1,47 @@
+package inheritance;
+/**
+ * @author paulinemakoma
+ * Assignment:A04 Inheritance
+ * Date:Nov 16, 2019
+ */
+public class IsoscelesRightTriangle {
+	
+	/**
+	 * 1 private final field of type int.
+	 */
+	private final int leg;
+
+	/**
+	 * parameterized constructor that initializes the field created about.
+	 * @param leg 
+	 */
+	public IsoscelesRightTriangle(int leg) {
+		super();
+		this.leg = leg;
+	}
+	
+	/**
+	 * Hypotenuse returns a value of type double
+	 * @param hypo
+	 * @return hypo value of hypotenuse
+	 */
+	public double hypotenuse(double hypo) {
+		return hypo;
+	}
+	
+	/**
+	 * getLeg provide a getter for the field but no setter
+	 * @return leg value of leg
+	 */
+	public int getLeg() {
+		return leg;
+	}
+	
+	/**
+	 * gives a String of the following form:
+	 * IsoscelesRightTriangle(leg) e.g. IsoscelesRightTriangle(5)
+	 */
+	public String toString() {
+		return String.format("IsoscelesRightTriangle(%d)", leg);
+	}
+}
---
+++ A04 Inheritance/src/inheritance/Square.java
@@ -0,0 +1,36 @@
+package inheritance;
+/**
+ * @author paulinemakoma
+ * Assignment:A04 Inheritance
+ * Date:Nov 16, 2019
+ */
+public class Square extends Rectangle {
+	
+	/**
+	 * parameterized constructor with only one parameter
+	 * The parameter is used to initialize both fields of Rectangle
+	 * @param a
+	 */
+	public Square(int a) {
+		super(a,a);
+
+	}
+	
+	/**
+	 * getSide expose the side-length of the square
+	 * @return length
+	 */
+	public int getSide() {
+		return super.getLength();
+	}
+	
+	/**
+	 * method that return a String of the following 
+	 * form: Square(side) e.g. Square(4) 
+	 */
+	@Override
+	public String toString() {
+		return String.format("Square(%d)", getLength());
+	}
+	
+}
